afd9257a0fba77f7769ff9951afffb3a4c6b9662
kjason1998 <kjason1998@gmail.com>
2020-10-13 11:36:04 +0700

    Update onClick delete

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,8 +4,8 @@ import "./Person.css";
 // using ES6
 const Person = (props)  => {
     return( 
-        <div className='Person'>
-            <p onClick={props.click}>My name is {props.name}! I amd {props.age} years old</p>
+        <div onClick={props.click} className='Person'>
+            <p>My name is {props.name}! I amd {props.age} years old</p>
             <p>{props.children}</p>
             <input type='test' onChange={props.changes} value={props.name}/>
         </div>
