aae89e03bb0dbde74e7a76784b909dd2425a5975
kjason1998 <kjason1998@gmail.com>
2020-10-14 08:20:02 +0700

    using styled-components on person

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import "./Person.css";
+import React from "react";
+import styled from "styled-components";
+//import "./Person.css"; - we use styled-component instead
+
+const DivStyled = styled.div`
+  width: 60%;
+  margin: 16px auto;
+  border: 1px solid #eeeeee;
+  box-shadow: 0 2px 3px #cccccc;
+  padding: 16px;
+  text-align: center;
+
+  @media (min-width: 500px) {
+    width: 450px;
+  }
+`; // inside `` is regular css
 
 // using ES6
-const Person = (props)  => {
-    const stylePerson = {
-        '@media (min-width: 500px)':{
-            width:'450px'
-        }
-    }
-
-    return( 
-        <div onClick={props.click} className='Person' style={stylePerson}>
-            <p>My name is {props.name}! I amd {props.age} years old</p>
-            <p>{props.children}</p>
-            <input type='test' onChange={props.changes} value={props.name}/>
-        </div>
-    )};
+const Person = (props) => {
+  return (
+    <DivStyled onClick={props.click}>
+      <p>
+        My name is {props.name}! I amd {props.age} years old
+      </p>
+      <p>{props.children}</p>
+      <input type="test" onChange={props.changes} value={props.name} />
+    </DivStyled>
+  );
+};
 
 export default Person;
 
